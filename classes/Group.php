ce2a9573043dffa036877e3c1499ef542fce4005
Amir TOLY <amir.toly@gmail.com>
2013-04-21 21:10:52 +0200

    FIx getters/setters

--- classes/Group.php
+++ classes/Group.php
@@ -13,13 +13,13 @@ class Group
     
     function __get($name) {
         if (property_exists($this, $name)) {
-            return $this->name;
+            return $this->$name;
         }
     }
     
     function __set($name, $value) {
         if (property_exists($this, $name)) {
-            $this->name = $value;
+            $this->$name = $value;
         }
         return $this;
     }
